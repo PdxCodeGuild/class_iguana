5501d9bcac7732249dd1b6c594ca050057d092ae
scottwstine <scottwstine@gmail.com>
2018-12-03 14:56:30 -0800

    Madlib - 1 of more

---
+++ Code/Scott/Python/lab02_madlib.py
@@ -0,0 +1,12 @@
+#lab 2 mad lib
+
+#Emergency Alert animal escapes from zoo
+#
+
+animal = input('Give me a type of animal:\n')
+city = input('Give the name of a city:\n')
+fur = input('Provide a color:\n')
+pounds = input('Provide a weight in pounds:\n')
+fluid = input('Provide a bodily fluid:\n')
+#
+print('Last night at 9 pm a ' + animal + ' escaped from the ' + city + ' zoo. Please be on the lookout. The ' + animal + ' has ' + fur + ' fur and weighs approximately '+pounds+' pounds. Please maintain your distance as the ' + animal + ' can emit ' + fluid + ' at long distances. ')
