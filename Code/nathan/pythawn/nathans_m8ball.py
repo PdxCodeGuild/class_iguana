b99aecaa2397fd854c4c97c7124a763f5733f13a
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-04 12:10:28 -0800

    emotikawn

--- Code/nathan/pythawn/nathans_m8ball.py
+++ Code/nathan/pythawn/nathans_m8ball.py
@@ -1,8 +1,10 @@
 import random
 
 magic_answers = ['head west', 'no', 'maybe', 'yes', 'indefinitely yes', 'subjectively no', 'subjectively yes', 'objectively no', 'ask again', 'hard pass']
-start_question = input('ask me something :    )\n absolutely anything will do \n >')
 
+user_input = input('ask me something,\n go ahead ask anything...')
 magic_answer = random.choice(magic_answers)
 print(magic_answer)
 print('\n*dont worry, someone loves you my friend*')
+
+
