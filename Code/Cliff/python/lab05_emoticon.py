09f5581a4dcbf9346ec0691baace42459ee455ec
Cliffnla <Cbnazphx@gmail.com>
2018-12-04 11:22:41 -0800

    started lab 5

---
+++ Code/Cliff/python/lab05_emoticon.py
@@ -0,0 +1,10 @@
+import random
+eyes = ['@@',';;'';',':','%']
+noses = ['>','#','*','-']
+mouths = ['(',')','0',]
+eyes = random.choice(eyes)
+noses = random.choice( noses)
+mouths = random.choice (mouths)
+print (eyes)
+print (noses)
+print (mouths)
