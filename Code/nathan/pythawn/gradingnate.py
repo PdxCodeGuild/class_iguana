b12dfe1107dcdf763b5181bb3353b6620462056c
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-04 10:01:16 -0800

    madlib

--- Code/nathan/pythawn/gradingnate.py
+++ Code/nathan/pythawn/gradingnate.py
@@ -1,33 +1,32 @@
 grade = input('what was your grade? \n>')
 grade = int(grade)
-
 if grade >= 90 and grade <=94:
     print('A-')
-if grade >= 80 and grade <=84:
+elif grade >= 80 and grade <=84:
     print('B-')
-if grade >= 70 and grade <=74:
+elif grade >= 70 and grade <=74:
     print('C-')
-if grade >= 60 and grade <=64:
+elif grade >= 60 and grade <=64:
     print('D-')
-if grade >= 50 and grade <=54:
-    print('F-')
-if grade <= 96 and grade >=100:
-    print('A+')
-if grade <= 86 and grade >=89:
+elif grade >= 50 and grade <=54:
+    print('F- oh no')
+elif grade <= 96 and grade >=100:
+    print('A+ wow')
+elif grade <= 86 and grade >=89:
     print('B+')
-if grade <= 76 and grade >=79:
+elif grade <= 76 and grade >=79:
     print('C+')
-if grade <= 66 and grade >=69:
+elif grade <= 66 and grade >=69:
     print('D+')
-if grade <= 56 and grade >=59:
+elif grade <= 56 and grade >=59:
     print('F+')
-if grade == 95:
+elif grade == 95:
     print('A')
-if grade == 85:
+elif grade == 85:
     print('B')
-if grade == 75:
+elif grade == 75:
     print('C')
-if grade == 65:
+elif grade == 65:
     print('D')
-if grade == 55:
+elif grade == 55:
     print('F')
