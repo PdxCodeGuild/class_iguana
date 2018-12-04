875452cd9d91164a1d949ad25c3d0170f4f2b5d9
Cliffnla <Cbnazphx@gmail.com>
2018-12-04 15:43:47 -0800

    lab 6 unfinished

--- Code/Cliff/python/Lab6.py
+++ Code/Cliff/python/Lab6.py
@@ -1,7 +1,10 @@
+n= input ( ' How many characters do you want in your password?')
+n= int(n)
+import random
 import string
-string.ascii_lowercase
-import string
-string.ascii_lowercase
-
-
+import string.digits
+count = 0
+while count < n:
+     print(), count
+count = count + 1
 
