069ed7e2f0af4b052ae011bb0ce8d98ecb867429
josh <jhell85@gmail.com>
2018-12-04 13:14:58 -0800

    update

--- Code/Josh/python/lab6_password_Generator.py
+++ Code/Josh/python/lab6_password_Generator.py
@@ -1,10 +1,10 @@
 import random
 import string
 length = int(input('how long would you like your password to be?\n'))
-char = string.ascii_letters + string.digits
+character = string.ascii_letters + string.digits
 password = 0
 new_pass = ''
 while password < length:
-    new_pass += random.choice(char)
+    new_pass += random.choice(character)
     password += 1
 print(f'{new_pass}')
