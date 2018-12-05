b87e7bb28f75b6de29c68106629d791a417c47c4
scottwstine <scottwstine@gmail.com>
2018-12-05 14:33:07 -0800

    lab 9 level 2

---
+++ Code/Scott/Python/lab_9_unit_conversion_level2.py
@@ -0,0 +1,22 @@
+#lab_9_unit_conversion_level_2
+
+#receive units and ocnverst to float
+input_distance = float(input('What is the distance?:\n'))
+input_unit = input('Measured in what?:\n')
+
+#create associations for measurement types.
+#round(length_in_meters, 4)
+if input_unit == 'mi':
+    miles = input_distance * 1609.34
+    miles = round(miles, 4)
+    print(f'{input_distance} mi is {miles} m')
+elif input_unit == 'ft':
+    feet = input_distance * 0.3048
+    feet = round(feet, 4)
+    print(f'{input_distance} ft is {feet} ft')
+elif input_unit == 'km':
+    kilo = input_distance * 1000
+    kilo = round(kilo, 4)
+    print(f'{input_distance} km is {kilo} m')
+
+exit()
