1c9e5ddd73af3524fb52a5ff54243df191e185d5
scottwstine <scottwstine@gmail.com>
2018-12-06 11:39:11 -0800

    password generator with random.shuffle rockpaperscissors with loop

--- Code/Scott/Python/lab6_password_generatory.py
+++ Code/Scott/Python/lab6_password_generatory.py
@@ -11,9 +11,6 @@ import string
 # print(f'Your new password is {password}')
 
 
-#2. get from user quantities of character types
-
-
 password_list = []
 
 lc = input('How many lower-case alphabet characters do you need?:\n')
@@ -21,58 +18,48 @@ lc = int(lc)
 lower_case_portion = ''
 for l in range(lc):
     lower_case_portion += random.choice(list(string.ascii_lowercase))
-    password_list.append(random.choice(string.ascii_lowercase))
+    #password_list.append(random.choice(string.ascii_lowercase))
 
 
 uc = input('How many upper-case alphabet characters do you need?:\n')
 uc = int(uc)
 upper_case_portion = ''
-for u in range(0, uc):
+for u in range(uc):
     upper_case_portion = upper_case_portion + random.choice(list(string.ascii_uppercase))
 
 num = input('How many numeric characters do you need?:\n')
 num = int(num)
 numeric_portion = ''
-for n in range(0, num):
+for n in range(num):
     numeric_portion = numeric_portion + random.choice(list(string.digits))
 
 char = input('How many special characters do you need?:\n')
 char = int(char)
 character_portion = ''
-for c in range(0, char):
+for c in range(char):
     character_portion = character_portion + random.choice(list(string.punctuation))
 
-# pull said quantities into a common list and combine with random.choice
-
 password_list = list(lower_case_portion) + list(upper_case_portion) + list(numeric_portion) + list(character_portion)
-# password_digits = list(lower_case_portion + upper_case_portion + numeric_portion + character_portion)
 
-
-# random.shuffle(password)
-
-#    0          1           2
-# ['apples', 'bananas', 'pears']
-
-
-# print(', '.join(['apples', 'bananas', 'pears']))
-#
-# print(''.join(password))
-
-#print(password_list)
+print(password_list) #this will show the list of all of the collected characters in order of their collection; ie, 2 by 2 or 3 by 3
 password = ''
 for i in range(len(password_list)):
-    random_index = random.randint(0, len(password_list)-1)
-    #print(password_list)
-    #print(random_index)
-    random_element = password_list.pop(random_index)
+    random_index = random.randint(0, len(password_list)-1) # variable for a randomly selected element (by index). to establish the proper range, use -1 because indexing starts with 0
+    print(password_list) #will show the same list as the one above first time through the loop, then each time missing a randomly selected element
+    # print(random_index)
+    random_element = password_list.pop(random_index) #this is removing whatever element was randomely seclected on line 47 via
 
-    #print(random_element)
+    # print(random_element)
     #print()
 
-    password += random_element
+    password += random_element # add random character to output
+
+    print(password)#this print statement (located here in the loop) will show the password growing by one element at a time.
+    print()
 
 print(f'Your new password is {password}')
-#
+
+# random.shuffle(password)
 
 
 
