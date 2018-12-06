c8829297856587c7eaba007216671c08f8e536e9
rs <rss.pdx@gmail.com>
2018-12-06 02:02:51 -0800

    Merge branch 'master' of https://github.com/PdxCodeGuild/class_iguana
    # Conflicts:
    #	Code/Richard/python/lab13-rot13.py
    #	Code/Richard/python/lab14-pick6.py

--- Code/Richard/python/lab13-rot13.py
+++ Code/Richard/python/lab13-rot13.py
@@ -1,68 +1,63 @@
 """
 author: Richard Sherman
 2018-12-05
-lab13-rot13.py, implements a rot-13 encoding
+lab13-rot13.py, implements a rot-13 encoding and a general rotation coding
+assumes input is ascii characters, digits and punctuation are unchanged
 """
 
 import string
 
-def rot13(s,tab):
-    x = s.translate(tab)
-    print(x)
-    return x
+def encode(s,dict):
+    s_out_list = []
+    for i in s:
+        s_i = dict[i]
+        s_out_list.append(s_i)
+    s_out = ''.join(s_out_list)
+    print(f'\n{s_out}')
+    return s_out
 
-def encode(s, tab):
-    x = s.translate(e_tab)
-    print(x)
-    return x
+ltr_lo_in = string.ascii_lowercase
+ltr_lo_out = string.ascii_lowercase[13:] + string.ascii_lowercase[:13]
 
-def decode(s, tab):
-    x = s.translate(d_tab)
-    print(x)
-    return x
+ltr_up_in = string.ascii_uppercase
+ltr_up_out = string.ascii_uppercase[13:] + string.ascii_uppercase[:13]
 
+ltr_in = ltr_lo_in + ltr_up_in + string.digits + string.punctuation
+ltr_out = ltr_lo_out + ltr_up_out + string.digits + string.punctuation
 
-low_1 = string.ascii_lowercase[:13]
-low_2 = string.ascii_lowercase[13:]
-up_1 = string.ascii_uppercase[:13]
-up_2= string.ascii_uppercase[13:]
-in_table = low_1 + up_1
-out_table = low_2 + up_2
-tab = str.maketrans(in_table, out_table)
-
+rot13_dict = dict(zip(ltr_in, ltr_out))
+print(rot13_dict)
 
 
 print('I\'ve learned a new trick. I can translate a message into a code so secret that only Caesar will be able to crack it. Are you ready to try it out?')
 s = input('Type something in, and I\'ll show you the secret code:  ')
 
-rot13(s, tab)
+encode(s, rot13_dict)
 
-print('=\nNow, perhaps you want a code so secret that not even Caesar can crack it.')
-print('For that, you\'ll have to enter a number: that\'s the key to the secret code.')
-key = input('Please enter your key:  ')
+print('\nNow, perhaps you want a code so secret that not even Caesar can crack it.')
+print('For that, you\'ll have to enter a number between 1 and 26 (inclusive): that\'s the key to the secret code.')
+key = int(input('Please enter your key:  '))
 s = input('Okay, now type in the message you want encoded:  ')
 
-e_low_1 = string.ascii_lowercase[:key]
-e_low_2 = string.ascii_lowercase[key:]
-e_up_1 = string.ascii_uppercase[:key]
-e_up_2 = string.ascii_uppercase[key:]
 
-d_low_1 = string.ascii_lowercase[key:]
-d_low_2 = string.ascii_lowercase[:key]
-d_up_1 = string.ascii_uppercase[key:]
-d_up_2 = string.ascii_uppercase[:key]
-in_e_table = e_low_1 + e_up_1
-out_e_table = e_low_2 + e_up_2
-in_d_table = d_low_1 + d_up_1
-out_d_table = d_low_2 + d_up_2
-e_tab = str.maketrans(in_e_table, out_e_table)
-d_tab = str.maketrans(in_d_table, out_d_table)
+ltr_lo_in = string.ascii_lowercase
+ltr_lo_out = string.ascii_lowercase[key:] + string.ascii_lowercase[:key]
 
-encode(s, e_tab)
+ltr_up_in = string.ascii_uppercase
+ltr_up_out = string.ascii_uppercase[key:] + string.ascii_uppercase[:key]
 
-input('\nIf you type "decode" and then the encoded message, you\'ll get the original message back:  ')
+ltr_in = ltr_lo_in + ltr_up_in + string.digits + string.punctuation
+ltr_out = ltr_lo_out + ltr_up_out + string.digits + string.punctuation
+
+encode_dict = dict(zip(ltr_in, ltr_out))
+decode_dict = dict(zip(ltr_out, ltr_in))
+
+encode(s, encode_dict)
+
+print('\nIf you type "decode" and then the encoded message, you\'ll get the original message back.')
 print('\nBut I\'ll save you the trouble. Let me decode the message for you.')
+print('I\'ll show you the coded message, then the decoded original.')
 
-decode(x, d_tab)
+encode(encode(s, encode_dict), decode_dict)
 
 
