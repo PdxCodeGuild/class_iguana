a66381aff8cc2cfaf72fbd22c641569f1830080d
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-07 14:46:43 -0800

    night core

---
+++ Code/nathan/pythawn/lab_10_average_numberv3.py
@@ -0,0 +1,15 @@
+numb = [5, 8, 3, 4, 9, 1, 3, 7, 6, 2]
+print(numb)
+numb.sort()
+print(numb)
+_index = len(numb) // 2
+
+if len(numb) % 2 == 1:
+    print(numb[_index // _index])
+elif len(numb) % 2 == 0:
+    print(numb[_index])
+
+
+
+
+
