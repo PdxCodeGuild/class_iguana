9930f15ed1f031aae77d95c830c4e6a252ecc7bd
rs <rss.pdx@gmail.com>
2018-12-07 11:21:49 -0800

    Merge branch 'master' of https://github.com/PdxCodeGuild/class_iguana
    # Conflicts:
    #	Code/Richard/python/lab13-rot13.py
    #	Code/Richard/python/lab14-pick6.py

--- Code/Richard/python/lab21-count-words.py
+++ Code/Richard/python/lab21-count-words.py
@@ -3,26 +3,42 @@ author: Richard Sherman
 2018-12-06
 lab21-count-words.py, counts words in a text and lists frequencies of word-occurrence
 """
-
+# make a list of the words, put their counts in a dict, sort to get the most frequent
 # note: change to use -with- when opening files
 
 import re
-import string   # unnecessary
+import string
 
 # version 1, using regexp
+
 ulysses_txt = open("ulysses.txt").read().lower()
 words = re.findall(r"\b[\w-]+\b", ulysses_txt)
 word_dict = {}
 for i in words:
-    word_dict[i] = word_dict.get(i, 0) + 1  # just barely understand .get method on dictionary
+    word_dict[i] = word_dict.get(i, 0) + 1
 print(word_dict)
 # list_from_word_dict = list(word_dict.keys())
 items = list(word_dict.items())
 #print(items)
-items.sort(key = lambda tup: tup[1], reverse = True)    # don't really understand this
-print(f'\n\n--->{items[0:10]}')
+items.sort(key = lambda tup: tup[1], reverse = True)
+print(f'\n\nThe most frequent words, with their frequencies, are:\n{items[0:10]}')
 
-# version 2: no idea how to collect word-pairs
+# version 2, collecting word pairs
+
+word_pairs = []
+for i in range(len(words)-1):
+    word_pairs.append((words[i], words[i+1]))
+word_pair_dict = {}
+for i in word_pairs:
+    word_pair_dict[i] = word_pair_dict.get(i, 0) + 1
+# print(word_pair_dict)
+# list_from_word_dict = list(word_dict.keys())
+items = list(word_pair_dict.items())
+#print(items)
+items.sort(key = lambda tup: tup[1], reverse = True)
+print(f'\n\nThe most frequent word pairs, with their frequencies, are:\n{items[0:10]}')
+
+
 
 # version 0, without regexp:    # note: don't understand why the results differ from those with regexp
 
@@ -33,9 +49,9 @@ words = ulysses_txt.split(' ')
 word_dict = {}
 for i in words:
     word_dict[i] = word_dict.get(i, 0) + 1
-print(word_dict)
+# print(word_dict)
 # list_from_word_dict = list(word_dict.keys())
 items = list(word_dict.items())
 #print(items)
 items.sort(key = lambda tup: tup[1], reverse = True)
-print(f'\n\n--->{items[0:10]}')
+print(f'\n\nThe most frequent words, with their frequencies, are:\n{items[0:10]}')
