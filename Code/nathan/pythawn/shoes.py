b110f459cf377132eae0045d418f76510cb30944
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-07 11:18:35 -0800

    night core

---
+++ Code/nathan/pythawn/shoes.py
@@ -0,0 +1,4 @@
+def shoes(shoe_type, shoe_color, shoe_size):
+    print(f"the shoes are {shoe_color}, they are {shoe_type} shoes, and they are size {shoe_size}")
+
+shoes('basketball', 'red and black', '9')
