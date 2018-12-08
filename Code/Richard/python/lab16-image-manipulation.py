4c5f535a9120a97ead7dc12d4ffd4ea5e64667d5
rs <rss.pdx@gmail.com>
2018-12-08 11:13:24 -0800

    pycharm problems

--- Code/Richard/python/lab16-image-manipulation.py
+++ Code/Richard/python/lab16-image-manipulation.py
@@ -15,7 +15,7 @@ for i in range(width):
         r, g, b = pixels[i, j]
 
         # your code here
-
+        (r, g, b) =  0.299*r + 0.587*g + 0.114*b
         pixels[i, j] = (r, g, b)
 
 img.show()
