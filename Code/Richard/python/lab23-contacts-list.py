40badd015ad3b99ab9750589fcce0f59dc634dad
rs <rss.pdx@gmail.com>
2018-12-08 04:49:52 -0800

    need advice on CRUD

--- Code/Richard/python/lab23-contacts-list.py
+++ Code/Richard/python/lab23-contacts-list.py
@@ -7,6 +7,7 @@ lab23-contacts-list.py, manipulates data from .csv file and implements a CRUD
 # the 'contacts list' here is just some data from the World Bank
 
 import csv
+import pandas as pd
 
 with open('world_data.csv', 'r') as file:
     lines = file.read().split('\n')
@@ -15,7 +16,7 @@ with open('world_data.csv', 'r') as file:
 varnames = (lines[0].split(','))
 print(varnames)
 
-# example of what the data (values) look like, without keys
+# split all lines into lists of values
 for i in range(len(lines)):
     lines[i] = (lines[i].split(','))
 # print(lines)
@@ -40,8 +41,23 @@ print(data_list[1]['Urban Population'])     # working ok
 data_list[1]['Maternal Mortality Rate'] = int(data_list[1]['Maternal Mortality Rate'])
 print(data_list[1])     # working ok
 
-# now destring and round all numerical values:
-for i in range(len(data_list)):     # not working
-    data_list[i]['Maternal Mortality Rate'] = int(data_list[i]['Maternal Mortality Rate'])
-    data_list[i]['Urban Population'] = float(data_list[i]['Urban Population'])
-    data_list[i]['GDP Per Capita'] = round(float(data_list[i]['GDP Per Capita']), 2)
+# # now destring and round all numerical values:  # not working
+# for i in range(len(data_list)):
+#     data_list[i]['Maternal Mortality Rate'] = int(data_list[i]['Maternal Mortality Rate'])
+#     data_list[i]['Urban Population'] = float(data_list[i]['Urban Population'])
+#     data_list[i]['GDP Per Capita'] = round(float(data_list[i]['GDP Per Capita']), 2)
+
+# alternative method using csv module
+# not working yet
+with open('world_data.csv', 'r', newline='\n') as file:
+    fieldnames = ['Country Name', 'Country Code', 'Maternal Mortality Rate', 'Urban Population', 'GDP Per Capita']
+    world_data = csv.DictReader(file, fieldnames=fieldnames)
+    dict_list = []
+    for row in world_data:
+        dict_list.append(row)
+print(dict_list)
+
+# alternative method using pandas
+world_data = pd.read_csv('world_data.csv')
+
+# implement a CRUD
