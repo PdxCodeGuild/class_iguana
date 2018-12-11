064f3adf2b83b61dc1920b8dde702c73095d314a
rs <rss.pdx@gmail.com>
2018-12-11 15:32:29 -0800

    cleaned up output

--- Code/Richard/python/lab-crime-data.py
+++ Code/Richard/python/lab-crime-data.py
@@ -1,3 +1,12 @@
+"""
+author: Richard Sherman
+2018-12-11
+lab-crime-data.py, calculates frequencies of Portland crime data from July 1972 - October 2018
+source is https://www.portlandoregon.gov/police/71978
+requires minor pre-processing (the variable 'Sex Offences, Nonforcible' has a comma in its name
+this was done outside of python)
+"""
+
 
 with open('crime_data.csv', 'r') as file:
     lines = file.read().split('\n')
@@ -36,11 +45,13 @@ for i in range(len(data_list)):
     data_list[i]['Occur Month'], data_list[i]['Occur Day'], data_list[i]['Occur Year'] = data_list[i]['Occur Date'].split('/')
 
 # look at the data again
-# print(f'first obs: {data_list[0]}')
+print(f'first obs: {data_list[0]}')
 # print(f'last obs: {data_list[-1]}')
 # print(f'1001th obs: {data_list[1001]}')
 # print(f'-1001th obs: {data_list[-1001]}')
 
+
+# count up crimes by a key, e.g. 'Offence Type' or 'Neighborhood'
 def get_count(key):
     count_dict = {}
     for i in range(len(data_list)):
@@ -52,14 +63,26 @@ def get_count(key):
 # get_count('Offense Type')
 # get_count('Occur Year')
 
+# sorts a dict by values, used here to sort the result of get_count(key)
 def count_freq(dict):
     items = list(dict.items())
     items.sort(key=lambda tup: tup[1], reverse=True)
 #    print(items)
     return items
-print('\n')
-print(f'The top ten crime types are: {count_freq(get_count("Offense Type"))[0:9]}')   # this works
-print(f'The top ten crime years are: {count_freq(get_count("Occur Year"))[0:9]}')
-print(f'The top ten crime Neighborhoods are {count_freq(get_count("Neighborhood"))[0:9]}')
 
-# count_freq(count_dict)    # this doesn't work
+print('\n')
+print(f'The top ten crime types are: {count_freq(get_count("Offense Type"))[0:9]}')
+print(f'The top ten crime years are: {count_freq(get_count("Occur Year"))[0:9]}')
+print(f'The top ten crime neighborhoods are {count_freq(get_count("Neighborhood"))[0:9]}')
+print(f'The top ten crime months are {count_freq(get_count("Occur Month"))[0:9]}')
+
+# # alternatively:
+# crime_type = get_count('Offence Type')
+# crime_year = get_count('Offence Year')
+# crime_hood = get_count('Neighborhood')
+# crime_month = get_count('Occur Month')
+#
+# print(f'The top ten crime types are: {crime_type[0:9]}')
+# print(f'The top ten crime years are: {crime_year[0:9]}')
+# print(f'The top ten crime neighborhoods are: {crime_hood[0:9]}')
+# print(f'The top ten crime months are: {crime_month[0:9]}')
