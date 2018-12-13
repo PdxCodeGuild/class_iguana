9892949c3860d90d51d29604f543a7a7b856d4f7
josh <jhell85@gmail.com>
2018-12-13 09:48:56 -0800

    updated

---
+++ 1 Python/code/Josh/lab16_image_manipulation.py
@@ -0,0 +1,11 @@
+from PIL import Image
+img = Image.open("ranbow.png") # must be in same folder
+width, height = img.size
+pixels = img.load()
+for i in range(width):
+    for j in range(height):
+        r, g, b = pixels[i, j]
+
+        # your code here
+
+        pixels[i, j] = (r, g, b)
