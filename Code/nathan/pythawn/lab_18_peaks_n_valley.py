d9ae9a1a120e14dbb2c63b8089ea9310c16c4b4f
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-14 09:49:55 -0800

    unreversed

--- Code/nathan/pythawn/lab_18_peaks_n_valley.py
+++ Code/nathan/pythawn/lab_18_peaks_n_valley.py
@@ -1,23 +1,21 @@
 def peaks_valleys(a):
     b = []
+    peaks = []
+    valleys = []
     for i in range(1, len(a)-1):
         if a[i-1] == a[i+1]:
             b.append(a[i])
-    return b
+    for i in range(1, len(a) - 1):
+        if a[i - 1] < a[i] and a[i + 1] < a[i]:
+            peaks.append(a[i])
+        if a[i - 1] > a[i] and a[i + 1] > a[i]:
+            valleys.append(a[i])
+    return b, peaks, valleys
 
 
 
 data = [1, 2, 3, 4, 5, 4, 3, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 5, 6, 7, 6, 5, 4, 3]
                     #^       ^                    ^               ^       ^
                     #p       v                    p               v       p
-peaks = []
-valleys = []
-print(peak(data))
-for i in range(1, len(data)-1):
-    if data[i-1] < data[i] and data[i+1] < data[i]:
-        peaks.append(data[i])
-    if data[i-1] > data[i] and data[i+1] > data[i]:
-        valleys.append(data[i])
-print(valleys)
-print(peaks)
+print(peaks_valleys(data))
 
