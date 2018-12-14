0a3f6f151f2368d6fb129e812f432c6e723c48cf
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-14 13:36:41 -0800

    unreversed

--- Code/nathan/pythawn/lab_21_count_words.py
+++ Code/nathan/pythawn/lab_21_count_words.py
@@ -1,10 +1,15 @@
+import string
+
 word_list = []
+repeat_words = []
 word_list = open('huckleberry.txt').read().split()
+
+for i in range(len(word_list)):
+    word_list[i] = word_list[i].strip(string.punctuation)
 
 for i in range(len(word_list)):
     word_list[i] = word_list[i].lower()
 
-repeat_words = []
 for i in range(len(word_list)):
     if word_list[i] == word_list[i]:
         repeat_words.append(word_list[i])
