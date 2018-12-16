88119b7013db91cc2cdb90993ba2b390cb1db40a
Richard Sherman <rss.pdx@gmail.com>
2018-12-16 12:12:46 -0800

    starting to build the class

---
+++ Code/Richard/python/lab25-atm.py
@@ -0,0 +1,31 @@
+"""
+author: Richard Sherman
+2018-12-16
+lab25-atm.py, an ATM simulator, an exercise in classes and functions
+"""
+
+import datetime
+
+class ATM(Object)
+    def __init__(self, balance, rate, period):
+        self.balance = balance
+        self.rate = rate
+        self.period = period
+
+    def check_withdrawal(amount):
+        if amount > balance:
+            return False
+        return True
+
+    def withdraw(amount):
+        if check_withdrawal:
+            balance = balance - amount
+
+    def deposit(amount):
+        balance = balance + amount
+
+    def check_balance():
+        return balance
+
+    def calc_interest():
+        now = datetime.now
