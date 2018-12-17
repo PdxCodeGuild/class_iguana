84f7f9a78eded82a7df56d25a13634e17063aa76
rs <rss.pdx@gmail.com>
2018-12-17 15:38:32 -0800

    need a loop to calculate interest based on system time

--- Code/Richard/python/lab25-atm.py
+++ Code/Richard/python/lab25-atm.py
@@ -6,7 +6,7 @@ lab25-atm.py, an ATM simulator, an exercise in classes and functions
 import datetime
 
 class ATM():
-    def __init__(self, balance, rate):
+    def __init__(self, balance = 100, rate = 0.01):
         self.balance = balance
         self.rate = rate
         self.transactions = []
@@ -17,20 +17,20 @@ class ATM():
         return True
 
     def withdraw(self, amount):
-        if check_withdrawal:
+        if self.check_withdrawal(amount):
             self.balance -= amount
-            transactions.append(f'Withdrawal of {amount} , {datetime.datetime(now)}, Balance = {self.balance} ')
+            self.transactions.append(f'Withdrawal of {amount}, {datetime.datetime.now()}, Balance = {self.balance} ')
         else:
             print('Insufficient funds')
 
 
     def deposit(self, amount):
         self.balance += amount
-        transactions.append(f'Deposit of {amount}, {datetime.datetime(now)}, Balance = {self.balance}')
+        self.transactions.append(f'Deposit of {amount}, {datetime.datetime.now()}, Balance = {self.balance}')
 
-    def check_balance(self, balance):
-        calc_interest(self.balance)
-        transactions.append(f'Interest of {self.rate} added to balance, {datetime.datetime(now)}, Balance = {self.balance}')
+    def check_balance(self):
+        self.calc_interest(self.balance)
+        self.transactions.append(f'Interest of {self.rate} added to balance, {datetime.datetime.now()}, Balance = {self.balance}')
         return self.balance
 
     def calc_interest(self, rate):
@@ -38,9 +38,9 @@ class ATM():
         return self.balance
 
     def print_transactions(self):
-        print(history)
+        print(self.transactions)
 
-
+atm = ATM()
 while True:
     command = input('what would you like to do (deposit, withdraw, check balance, history, exit)? ')
     if command == 'exit':
@@ -48,11 +48,11 @@ while True:
         break
     elif command == 'deposit':
         amount = float(input('what is the amount you\'d like to deposit? '))
-        ATM.deposit(amount)
+        atm.deposit(amount)
     elif command == 'withdraw':
         amount = float(input('what is the amount you\'d like to withdraw? '))
-        ATM.withdraw(amount)
+        atm.withdraw(amount)
     elif command == 'check balance':
-        print(f'Your account balance is ${ATM.check_balance()}')
+        print(f'Your account balance is ${atm.check_balance()}')
     elif command == 'history':
-        ATM.print_transactions()
+        atm.print_transactions()
