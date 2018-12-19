e3357df8adaad839fc359e536db3c782888b3645
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-19 14:31:44 -0800

    unreversed

---
+++ Code/nathan/pythawn/lab_25_atm.py
@@ -0,0 +1,46 @@
+def check_balance(account):
+    return account
+
+def check_interest(account, interest):
+    return account * interest
+
+
+def deposit(account, amount):
+    return account + amount
+
+
+def withdrawl(account, amount):
+    if account - amount > -1:
+        x = account - amount
+    else:
+        if account - amount < 0:
+            print('sorry you dont have that much money')
+    return x
+
+
+account = 0
+interest = 0.1
+amount = 0
+while True:
+    transaction = input('would you like to check balance, check interest, make a deposit, or make a withdrawl?\n**type finish if finished**\n')
+    if transaction == 'check balance':
+        print(check_balance(account))
+    if transaction == 'deposit':
+        amount = int(input('how much would you like to deposit?\n'))
+        account = (deposit(account, amount))
+        print(account)
+    if transaction == 'withdrawl':
+        amount = int(input('how much money do you want to take out?\n'))
+        account = (withdrawl(account, amount))
+        print(account)
+    if transaction == 'check interest':
+        print(check_interest(account, interest))
+    if transaction == 'finish':
+        break
+
+
+
+
+
+
+
