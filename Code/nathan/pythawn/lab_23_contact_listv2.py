43583c4f091331a03936d55e511c6e950e5717d5
rodgnathaniel <rodgnathaniel@gmail.com>
2018-12-19 12:17:13 -0800

    unreversed

--- Code/nathan/pythawn/lab_23_contact_listv2.py
+++ Code/nathan/pythawn/lab_23_contact_listv2.py
@@ -16,21 +16,16 @@ for in_list in data:
     contacts.append(contact)
 
 #adding a contact
-new_contact = []
-i = input('name? \n')
-color = input('favorite color?\n')
-food = input('favorite food?\n')
-animal = input('favorite animal?\n')
-new_contact.append(i)
-new_contact.append(color)
-new_contact.append(food)
-new_contact.append(animal)
-print(new_contact)
-
 new_contact_dict = {}
+new_contact = []
+for key in key_list:
+    value = input('what is new contacts '+key+'?')
+    new_contact.append(value)
 for i in range(len(key_list)):
     new_contact_dict[key_list[i]] = new_contact[i]
 contacts.append(new_contact_dict)
+
+print(new_contact)
 
 print(contacts)
 
@@ -52,3 +47,21 @@ for i in range(len(contacts)):
         if j == 'name' and (contacts[i]['name']) == contact_delete:
             contacts.pop(i)
 print(contacts)
+
+#update a contacts info
+# update = input('who would you like to input?\n')
+# update_which_key = ''
+# for i in range(len(contacts)):
+#     for j in contacts[i]:
+#         if j == 'name' and (contacts[i]['name']) == update:
+#             update_which_key = input('would you like to change name, fav_food, fav_color, or animal?\n')
+#             if update_which_key == 'name':
+#
+#             else:
+#                 if update_which_key == 'fav_food':
+#
+#             else:
+#                 if update_which_key == 'fav_color':
+#
+#             else:
+#                 if update_which_key == 'animal':
