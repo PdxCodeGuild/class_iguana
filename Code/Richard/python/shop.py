458f2e41fc73194dc4118d6feec7e8ccc6d083e1
rs <rss.pdx@gmail.com>
2018-12-20 13:15:51 -0800

    need to learn how to pretty print

--- Code/Richard/python/shop.py
+++ Code/Richard/python/shop.py
@@ -1,3 +1,10 @@
+"""
+author: Richard Sherman
+2018-12-20
+shop.py, a.k.a. lab25-atm-v2.py, simulates a shop selling 4 items of clothing,
+an exercise in classes and functions
+"""
+
 import random
 import datetime
 
@@ -9,27 +16,35 @@ class Shop():
         self.transactions = []
 
     def restock(self):
-        print(f'The current inventory is {self.stock}.')
+        print(f'\nThe current inventory is {self.stock}.')
         stock_not_stock = input('Would you like to re-stock? y or n:  ')
         if 'y' in stock_not_stock:
-            to_stock = input('What would you like to re-stock? shirts, pants, shoes, or socks:  ')
-            qty_to_stock = int(input(f'How many new {to_stock} do want?'  ))
-            self.stock[to_stock] += qty_to_stock
-            self.cash -= qty_to_stock * self.price[to_stock]
-            self.transactions.append(f'Stocked {qty_to_stock} {to_stock}, {datetime.datetime.now()}, Cash on hand = {self.cash} ')
+            to_stock = input('\nWhat would you like to re-stock? shirts, pants, shoes, or socks:  ')
+            qty_to_stock = int(input(f'\nHow many new {to_stock} do want?  '))
+            if self.cash >= qty_to_stock * self.price[to_stock]:
+                self.stock[to_stock] += qty_to_stock
+                self.cash -= qty_to_stock * self.price[to_stock]
+                self.transactions.append(f'Stocked {qty_to_stock} {to_stock}, {datetime.datetime.now()}, Cash on hand = {self.cash} ')
+            else:
+                print('We don\'t have enough cash on hand. We\'ll have to restock later.')
 
     def sell(self):
-        item_to_sell = input('There\'s a customer in the shop. What would you like to sell? shirts, pants, shoes, or socks:  ')
+        item_to_sell = input('\nThere\'s a customer in the shop. What would you like to sell? shirts, pants, shoes, or socks:  ')
         offer = round(random.uniform(price[item_to_sell] - 1, price[item_to_sell] * 2), 2)
         sell_not_sell = input(f'The price they\'re offering is {offer}. Would you like to take that offer? y or n:  ')
         if 'y' in sell_not_sell:
             qty_to_sell = int(input('How many do you want to sell? 1, 2, 3 ...?  '))
+            if stock[item_to_sell] < qty_to_sell:
+                if stock[item_to_sell] == 0:
+                    print('We don\'t have any in stock.')
+                else:
+                    print(f'We have {stock[item_to_sell]} on hand. We\'ll sell those now and then restock.')
             self.cash += offer * qty_to_sell
             self.stock[item_to_sell] -= qty_to_sell
             self.transactions.append(f'Sold {qty_to_sell} {item_to_sell}, {datetime.datetime.now()}, Cash on hand = {self.cash} ')
 
     def check_books(self):
-        print(self.transactions)
+        print(f'\n{self.transactions}')
 
 
 stock = {'shirts': 10,
@@ -43,6 +58,9 @@ price = {'shirts': 3,
          'socks':  1}
 
 shop = Shop(stock, price, 100)
+
+print('\nThe shop\'s open!\n')
+print('When it\'s time to close, just type q.\n')
 
 while True:
     shop.sell()
