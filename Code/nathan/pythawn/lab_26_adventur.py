170d0b40ce8abda72276677bbab441ad19470f4f
rodgnathaniel <rodgnathaniel@gmail.com>
2019-01-02 14:01:57 -0800

    unreversed

--- Code/nathan/pythawn/lab_26_adventur.py
+++ Code/nathan/pythawn/lab_26_adventur.py
@@ -23,7 +23,7 @@ for i in range(4):
     board[enemy_i][enemy_j] = '§'
 
 # loop until the user says 'done' or dies
-weapons = ['six shooter', 'ray gun', 'magnet gun']
+weapons = ['1', '2', '3']
 coin = ['h', 't']
 while True:
 
@@ -51,65 +51,63 @@ while True:
     # check if the player is on the same space as an enemy
     if board[player_i][player_j] == '§':
         print('you\'ve encountered an enemy!')
-        action = input('what will you do? ')
-        if action == 'attack':
-            weapon_choice = input('choose a weapon (six shooter, ray gun, magnet gun)')
-            enemy_weapon = random.choice(weapons)
-            coin_flip = random.choice(coin)
-            if weapon_choice == 'six shooter' and enemy_weapon == 'six shooter':
-                if coin_flip == 't':
-                    board[player_i][player_j] = ' '  # remove the enemy from the board
-                    kill_count += 1
-                    print('you won the quick draw')
-                else:
-                    if coin_flip == 'h':
-                        print('you lost the quick draw')
-                        break
-            elif weapon_choice == 'six shooter' and enemy_weapon == 'ray gun':
-                print('you were demolished by the enemies ray gun')
-                break
-            elif weapon_choice == 'six shooter' and enemy_weapon == 'magnet gun':
-                print('a six shooter is no match for a magnet gun')
-                break
-            elif weapon_choice == 'ray gun' and enemy_weapon == 'six shooter':
-                print('the enemy died from your ray gun attack')
+        weapon_choice = input('choose a weapon [1]six shooter, [2]ray gun, [3]magnet gun)')
+        enemy_weapon = random.choice(weapons)
+        coin_flip = random.choice(coin)
+        if weapon_choice == '1' and enemy_weapon == '1':
+            if coin_flip == 't':
                 board[player_i][player_j] = ' '  # remove the enemy from the board
                 kill_count += 1
-            elif weapon_choice == 'ray gun' and enemy_weapon == 'ray gun':
-                if coin_flip == 't':
-                    board[player_i][player_j] = ' '
-                    kill_count += 1
-                    print('you won the quick draw')
-                else:
-                    if coin_flip == 'h':
-                        print('you lost the quick draw')
-                        break
-
-            elif weapon_choice == 'ray gun' and enemy_weapon == 'magnet gun':
-                print('a ray gun is no match for a magnet gun')
-                break
-            elif weapon_choice == 'magnet gun' and enemy_weapon == 'six shooter':
-                print('the enemy died')
-                board[player_i][player_j] = ' '  # remove the enemy from the board
-                kill_count += 1
-            elif weapon_choice == 'magnet gun' and enemy_weapon == 'ray gun':
-                print('nice')
+                print('you won the quick draw')
+            else:
+                if coin_flip == 'h':
+                    print('you lost the quick draw')
+                    break
+        elif weapon_choice == '1' and enemy_weapon == '2':
+            print('you were demolished by the enemies ray gun')
+            break
+        elif weapon_choice == '1' and enemy_weapon == '3':
+            print('a six shooter is no match for a magnet gun \n you were murked')
+            break
+        elif weapon_choice == '2' and enemy_weapon == '1':
+            print('the enemy died from your ray gun attack')
+            board[player_i][player_j] = ' '  # remove the enemy from the board
+            kill_count += 1
+        elif weapon_choice == '2' and enemy_weapon == '2':
+            if coin_flip == 't':
                 board[player_i][player_j] = ' '
                 kill_count += 1
-            elif weapon_choice == 'magnet gun' and enemy_weapon == 'magnet gun':
-                if coin_flip == 't':
-                    board[player_i][player_j] = ' '
-                    kill_count += 1
-                    print('you won the quick draw')
-                    break
-                else:
-                    if coin_flip == 'h':
-                        print('you lost the quick draw')
-                        break
-
+                print('you won the quick draw')
             else:
-                print('you hestitated and were slain')
+                if coin_flip == 'h':
+                    print('you lost the quick draw')
+                    break
+
+        elif weapon_choice == '2' and enemy_weapon == '3':
+            print('a ray gun is no match for a magnet gun \n you got owned')
+            break
+        elif weapon_choice == '3' and enemy_weapon == '1':
+            print('the enemy died thanks to your speedy fast magnet gun')
+            board[player_i][player_j] = ' '  # remove the enemy from the board
+            kill_count += 1
+        elif weapon_choice == '3' and enemy_weapon == '2':
+            print('nice')
+            board[player_i][player_j] = ' '
+            kill_count += 1
+        elif weapon_choice == '3' and enemy_weapon == '3':
+            if coin_flip == 't':
+                board[player_i][player_j] = ' '
+                kill_count += 1
+                print('you won the quick draw')
                 break
+            else:
+                if coin_flip == 'h':
+                    print('you lost the quick draw')
+                    break
+
+        else:
+            print('you were slain')
+            break
 
             # print out the board
     for i in range(height):
