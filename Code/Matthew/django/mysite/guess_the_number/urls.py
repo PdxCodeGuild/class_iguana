b2762ecb63d93547f203c91d4272ec1033a70a82
flux2341 <flux2341@gmail.com>
2019-02-08 16:16:16 -0800

    guess the number - complete

--- Code/Matthew/django/mysite/guess_the_number/urls.py
+++ Code/Matthew/django/mysite/guess_the_number/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 app_name = 'guess_the_number'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:game_id>/', views.game, name='game'),
+    path('<int:game_id>/guess', views.guess, name='guess'),
+    path('newgame/', views.newgame, name='newgame'),
 ]
