49541922c769194e981377c82482bd7d7c64a534
guam68 <guam_68@hotmail.com>
2019-02-11 16:49:35 -0800

    lab04 login/signup forms

--- Code/Daniel/django/lab04_library/library/urls.py
+++ Code/Daniel/django/lab04_library/library/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name='library'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('newuser/', views.newuser, name='newuser'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login, name='login'),
 ]
