38c50c6f212bd3ca2f7a03f96334a70e06133936
Richard Sherman <rss.pdx@gmail.com>
2019-02-14 17:48:33 -0800

    continue restart of bank

--- Code/Richard/django/bank/acme_bank/views.py
+++ Code/Richard/django/bank/acme_bank/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import Account
 
 def account_detail(request, account_id):
-    print(account_id)
     account = Account.objects.get(id=account_id)
     print(account.balance)
     return render(request, 'acme_bank/account_detail.html', {'account': account})
